25eb480c8466763b5bed6f4b1f0a05006e821212
Yoel Habtit <yoel.habtit@learnet.se>
2023-01-12 10:23:07 +0100

    Attack 1

--- Projekt_1.java
+++ Projekt_1.java
@@ -7,20 +7,22 @@ import java.util.Scanner;
         public static void main(String[] args){
             Scanner Rambo = new Scanner (System.in);
 
-            System.out.println("Välkommen till(...)");
-            System.out.print("Välj ett namn: ");
+            System.out.println("Welcome to(...)");
+            System.out.print("Choose a name: ");
             String Name = Rambo.nextLine();
-            System.out.println("Hej "+Name);
+            System.out.println("Hello, "+Name+"! Press enter to begin...");
+            System.out.println("You are a member of the notorious ");
+            
 
-            Player_Attacks(Move);
+            Player_Attacks();
 
         }
 
-        static String Player_Attacks(String Move)
+        static boolean Player_Attacks()
         {
             Scanner Laeeb = new Scanner(System.in);
 
-            System.out.println("Move: ");
+            System.out.print("Move: ");
             String moves = Laeeb.nextLine();
 
             if(moves.equals("Special Attack"))
@@ -29,12 +31,38 @@ import java.util.Scanner;
 
                 if(95<=toffla && toffla<=100)
                 {
-                    System.out.println("critical hit");
-                }
+                    System.out.println("Critical hit!!!");
 
+                    return true;
+                }
+                else
+                {
+                    System.out.println("Special Attack Failed!");
+                    return false;
+                }
+                
             }  
-                moves = Move;
-                return Move;
+
+            if(moves.equals("Attack 1"))
+            {
+                double toffla = Math.random()*100;
+
+                if(10<=toffla && 100>=toffla)
+                {
+                    System.out.println("Hit!!!");
+
+                    return true;
+                }
+                else
+                {
+                    System.out.println("Miss!!!");
+
+                    return false;
+                }
+            }
+
+            return false;
+
         }
 
         
