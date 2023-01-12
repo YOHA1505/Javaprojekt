9880c477c4488ed440b3d4178b5785dfa1014e07
Yoel Habtit <yoel.habtit@learnet.se>
2023-01-12 11:04:08 +0100

    Attack 2, HP, intro to story

--- Projekt_1.java
+++ Projekt_1.java
@@ -3,7 +3,8 @@ package Projekt_1;
 import java.util.Scanner;
 
     public class Projekt_1 {
-            
+            static int Player_HP = 100;
+            static int Opp_HP = 100;
         public static void main(String[] args){
             Scanner Rambo = new Scanner (System.in);
 
@@ -11,10 +12,14 @@ import java.util.Scanner;
             System.out.print("Choose a name: ");
             String Name = Rambo.nextLine();
             System.out.println("Hello, "+Name+"! Press enter to begin...");
-            System.out.println("You are a member of the notorious ");
+            System.out.println("You are a member of the notorious gang Los Vagos in the southside of the city Rondua");
             
 
             Player_Attacks();
+            if(Opp_HP==0)
+            {
+                System.out.println("Opp has been obliterated");
+            }
 
         }
 
@@ -32,6 +37,7 @@ import java.util.Scanner;
                 if(95<=toffla && toffla<=100)
                 {
                     System.out.println("Critical hit!!!");
+                    Opp_HP=-100;
 
                     return true;
                 }
@@ -47,7 +53,25 @@ import java.util.Scanner;
             {
                 double toffla = Math.random()*100;
 
-                if(10<=toffla && 100>=toffla)
+                if(10<=toffla && toffla<=100)
+                {
+                    System.out.println("Hit!!!");
+
+                    return true;
+                }
+                else
+                {
+                    System.out.println("Miss!!!");
+
+                    return false;
+                }
+            }
+
+            if(moves.equals("Attack 2"))
+            {
+                double toffla = Math.random()*100;
+
+                if(25<=toffla && toffla<=100)
                 {
                     System.out.println("Hit!!!");
 
